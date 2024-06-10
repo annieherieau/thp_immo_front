0201fb8e46d59cd6c7693a26d79fafe8c9faef5c
annieherieau <annie.herieau@gmail.com>
2024-06-10 10:49:55 +0200

    afficher email du user

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -7,7 +7,7 @@ export default function ListingCard({ listingData, preview = true }) {
       <h4>{listing.title}</h4>
       <p>{listing.description}</p>
       <p>{listing.price} €</p>
-      {user_email && (<p>{user_email}</p>)}
+      {user_email && !preview && (<p>{user_email}</p>)}
       <div>
         {preview && <Link to={`/listing/${listing.id}`}>Voir l'annonce</Link>}
       </div>
