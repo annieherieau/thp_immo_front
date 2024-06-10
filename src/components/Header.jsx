b063e21ddb3e4070f7c783f7cf0bde8869728873
annieherieau <annie.herieau@gmail.com>
2024-06-10 16:00:24 +0200

    menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,6 +36,7 @@ export default function Header() {
           <>
             <li>
               <NavLink to="/profile">Mon profile</NavLink>
+              <NavLink to="/userinfos">Mes informations</NavLink>
             </li>
             <li>
               <button onClick={handleLogout}>Se déconnecter</button>
