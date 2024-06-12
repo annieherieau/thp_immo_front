41f4ff1bdc96e5af36eee1289167daa85cfc62e6
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-06-12 15:07:25 +0200

    editing listing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 export default function Home() {
   return (
     <div>
-      <h1>La Marketplace de l'immo</h1>
+      <h1>La Marketplace de l&apos;immo</h1>
       <Hero />
       <h1>Liste des biens immobiliers</h1>
       <ListingsIndex/>
